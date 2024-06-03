85b3032726c97c6010ebfaf5b78fdfd70e17b863
ssu0629 <ssu0629@naver.com>
2024-06-03 12:56:34 +0900

    Update movescrew.js

--- movescrew.js
+++ movescrew.js
@@ -160,9 +160,6 @@ class Game {
       if (screw.isMouseOver()) { // 마우스가 나사 위에 있을 때
         this.selectedScrew = screw; // 나사 선택
         this.mode = "rotate"; // 모드 변경
-        this.previousDeg = totalDeg;
-        this.previousDeg_1 = abs(totalDeg);
-        this.previousDeg_2 = radians(90) - abs(totalDeg);
         break;
       }
     }
