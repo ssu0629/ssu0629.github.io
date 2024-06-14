b8b87cd9c398264dacb07110e66235658f25a3d2
ssu0629 <ssu0629@naver.com>
2024-06-14 15:49:52 +0900

    Update movescrew.js

--- movescrew.js
+++ movescrew.js
@@ -18,6 +18,67 @@ let det = false
 
 let t = 0, dt = 1;
 
+let shared;
+let clickCount;
+
+let guests;
+let me;
+let game;
+let checkpointPassed = [false, false, false]; // 체크포인트 통과 여부를 저장
+let rotationCount = 0; // 회전 수를 저장
+let screwImgs = new Array(8);
+let screwSelectedImgs = new Array(8);
+let screwBgImg;
+let introImg;
+let gameState;
+
+let centerX, centerY;
+let totalDeg, pTotalDeg;
+let halfCount = 0
+let count = 0;
+let pCount
+let pANum = 0;
+let areaNum = 0;
+let clockWise = {
+  1: false,
+  2: false,
+  3: false,
+};
+let antiClockWise = {
+  1: false,
+  2: false,
+  3: false,
+};
+
+
+
+// DOMContentLoaded 이벤트 리스너를 추가하여 HTML 문서가 완전히 로드된 후 onClick 함수를 버튼 클릭 이벤트에 연결
+document.addEventListener("DOMContentLoaded", function () {
+  const activateButton = document.getElementById('activateButton');
+  if (activateButton) {
+    activateButton.addEventListener('click', onClick);
+  } else {
+    console.error("Activate button not found.");
+  }
+});
+
+// // onClick 함수는 iOS 기기에서 motion 권한을 요청합니다.
+// function onClick() {
+//   if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+//     DeviceOrientationEvent.requestPermission()
+//       .then(permissionState => {
+//         if (permissionState === 'granted') {
+//           window.addEventListener('deviceorientation', cb);
+//         }
+//       })
+//       .catch(console.error);
+//   } else {
+//     window.addEventListener('deviceorientation', cb);
+//     // iOS 13 이전 버전이나 다른 장치에서는 권한 요청 없이 바로 이벤트를 추가
+//   }
+// }
+
+
 function cb(event) {
   dthetaXdt = event.rotationRate.alpha * PI / 180;
   dthetaYdt = event.rotationRate.beta * PI / 180;
